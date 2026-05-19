6c5ed8fd64c0582ad723e8874e42c0aa3fff5073
Y0LRIN <chloe.arcadia.alx@gmail.com>
2026-05-19 16:58:26 +0200

    (WIP)AgentController.java

---
+++ backend/controller/AgentController.java
@@ -0,0 +1,85 @@
+package controller;
+
+import db.AgentRepository;
+import model.Address;
+import model.Agent;
+import util.HttpUtils;
+import util.Json;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.org.apache.bcel.internal.classfile.Unknown;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class AgentController {
+
+  private final AgentRepository repo = new AgentRepository();
+
+  public void handle(HttpExchange ex) throws IOException {
+    int id = HttpUtils.segmentInt(ex, 3);
+
+    try {
+      switch (ex.getRequestMethod()) {
+        case "GET" -> {
+          if (id > 0) {
+            getOne(ex, id);
+          } else {
+            getAll(ex);
+          }
+        }
+        case "POST" -> create(ex);
+        case "PUT" -> update(ex, id);
+        case "DELETE" -> delete(ex, id);
+        default -> HttpUtils.sendJson(ex, 405, "Unknown method");
+      }
+    } catch (Exception e) {
+      HttpUtils.sendJson(ex, 500, Json.error(e.getMessage()));
+    }
+  }
+
+  // HANDLERS
+
+  private void getAll(HttpExchange ex) throws Exception {
+    List<Agent> agents = repo.findAll();
+    List<Map<String, Object>> list = agents.stream().map(this::toMap).toList();
+    HttpUtils.sendJson(ex, 200, Json.toJson(list));
+  }
+
+  private void getOne(HttpExchange ex, int id) throws Exception {
+    Optional<Agent> agent = repo.findbyId(id);
+    if (agent.isEmpty()) {
+      HttpUtils.sendJson(ex, 404, "Unknown agent");
+      return;
+    }
+    HttpUtils.sendJson(ex, 200, Json.toJson(toMap(agent.get())));
+  }
+
+  private void create(HttpExchange ex) throws Exception {
+    Map<String, String> body = Json.parse(HttpUtils.readBody(ex));
+    String name = body.get("name");
+    String email = body.get("email");
+    String phone = body.get("phone");
+    String is_adminStr = body.get("is_admin");
+    String created_at = body.get("created_at");
+    if (name == null || email == null || phone == null || is_adminStr == null || created_at == null) {
+      HttpUtils.sendJson(ex, 400, Json.error("All fields required (name/email/phone/is_admin/created_at)"));
+      return;
+    }
+    boolean is_admin = Boolean.parseBoolean(is_adminStr);
+    Agent created = repo.create(name, email, phone, is_admin, created_at);
+    HttpUtils.sendJson(ex, 201, Json.toJson(toMap(created)));
+  }
+
+  private void update(HttpExchange ex, int id) throws Exception {
+    if (id <= 0) {
+      HttpUtils.sendJson(ex, 400, Json.error("Invalid ID"));
+      return;
+    }
+
+    Map<String, String> body = Json.parse(HttpUtils.readBody(ex));
+
+  }
+}
